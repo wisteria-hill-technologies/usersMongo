a55b3a4ee9654a316126cb68fb06713923b48e25
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-06-13 11:59:41 +0100

    adds reading_test.js. also use nodemon to run test.

--- test/reading_test.js
+++ test/reading_test.js
@@ -18,6 +18,13 @@ describe('Reading users out of the databse', ()=>{
         assert(users[0]._id.toString() === joe._id.toString());
         done();
       });
-
   });
+
+  it('find a user with a particular id', (done)=> {
+    User.findOne({ _id: joe._id })
+      .then((user)=>{
+        assert(user.name === "Joe");
+        done();
+      });
+  })
 });
