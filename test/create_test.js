f11aa4afe33c1cc72a05b2d68d6980ed2517ba8c
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-06-13 10:47:15 +0100

    creates first test for saving a user

---
+++ test/create_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating records', ()=>{
+  it('saves a user', (done)=>{ //add done so that other codes outside it will not be executed until 'it' is done.
+    const joe = new User({
+      name: "Joe"
+    });
+    joe.save()
+      .then(()=>{
+        // Has joe been saved successfully?
+        // Check if Joe is saved below with assert.
+        assert(!joe.isNew); //.isNew means the data exists but not saved in the database yet.
+        done();  //sinal with 'done()'' that this 'it' funciton is complete, and that we can move on to next test.
+      });
+
+  });
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', ()=> console.log('Good to go!'))
-  .on('error', (error)=>{
-    console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+
+before((done)=>{  //before will run the code inside only once.
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', ()=> {
+      //Once the connection is open, it will run the function.
+      done(); //call this only when mongoose connects to mongoDB successfully.
+    })
+    .on('error', (error)=>{  //on is run when mongo emits an error.
+      console.warn('Warning', error);
+    });
+});
+
+beforeEach((done)=>{ //beforeEach runs code insdie before each test.
+  // clear users in database with drop()
+  mongoose.connection.collections.users.drop(()=>{
+    // Ready to run the next test!
+    done(); //use done here so that the next test will be executed only after the users in the database are deleted.
   });
+});
