efdfb5ec3e0c121640d8e9b9d1bc0c564b3430df
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-06-14 11:58:09 +0100

    adds subdocuments Posts to User model.

--- src/user.js
+++ src/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const PostSchema = require('./postSchema');
 
 const UserSchema = new Schema({
   name: {
@@ -10,7 +11,8 @@ const UserSchema = new Schema({
     },
     required: [true, 'Name is required.']
   },
-  postCount: Number
+  postCount: Number,
+  posts: [PostSchema]  //To add subdocuments, just do like this.
 });
 
 const User = mongoose.model('User', UserSchema);
---
+++ test/subdocument_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Subdocuments', ()=> {
+  it('can create a subdocument', (done) => {
+    const joe = new User({ name: 'Joe', posts: [{ title: 'MyPostTitle' }] });
+    joe.save()
+      .then(()=>User.findOne({ name: 'Joe'}))
+      .then((user)=>{
+        assert(user.posts[0].title==="MyPostTitle");
+        done();
+      });
+  });
+});
